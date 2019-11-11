f1b33682849a324490fd6c4f63dca30ba620e934
Jon Finley <lilfin19@gmail.com>
2019-11-11 17:00:24 -0600

    html routes linked

--- routes/Campaigns-api-routes.js
+++ routes/Campaigns-api-routes.js
@@ -1,25 +1,24 @@
 var db = require("../models");
 
-module.exports = function(app) {
-    // Get all Campaigns
-    app.get("/api/Campaigns", function(req, res) {
-      db.Campaigns.findAll({}).then(function(dbCampaigns) {
-        res.json(dbCampaigns);
-      });
+module.exports = function (app) {
+  // Get all Campaigns
+  app.get("/api/Campaigns", function (req, res) {
+    db.Campaigns.findAll({}).then(function (dbCampaigns) {
+      res.json(dbCampaigns);
     });
-  
-    // Create a new Campaigns
-    app.post("/api/Campaigns", function(req, res) {
-      db.Campaigns.create(req.body).then(function(dbCampaigns) {
-        res.json(dbCampaigns);
-      });
+  });
+
+  // Create a new Campaigns
+  app.post("/api/Campaigns", function (req, res) {
+    db.Campaigns.create(req.body).then(function (dbCampaigns) {
+      res.json(dbCampaigns);
     });
-  
-    // Delete an Campaigns by id
-    app.delete("/api/Campaigns/:id", function(req, res) {
-      db.Campaigns.destroy({ where: { id: req.params.id } }).then(function(dbCampaigns) {
-        res.json(dbCampaigns);
-      });
+  });
+
+  // Delete an Campaigns by id
+  app.delete("/api/Campaigns/:id", function (req, res) {
+    db.Campaigns.destroy({ where: { id: req.params.id } }).then(function (dbCampaigns) {
+      res.json(dbCampaigns);
     });
-  };
-  
+  });
+};
